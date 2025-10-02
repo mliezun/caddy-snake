aed233255fa39fd0d219d60bda0db0f297cddd16
Miguel Liezun <muragnarokstaff@gmail.com>
2025-10-02 10:57:23 +0200

    Add main package for embedding and extracting Python standalone package and Caddy binary

---
+++ cmd/embed/main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"archive/tar"
+	"bytes"
+	_ "embed"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/klauspost/compress/zstd"
+)
+
+//go:embed python-standalone.tar.zst
+var pythonStandalonePkg []byte
+
+//go:embed caddy
+var caddyBinary []byte
+
+// extractTarZst extracts an embedded tar.zst into a target directory
+func extractTarZst(data []byte, targetDir string) error {
+	zsr, err := zstd.NewReader(bytes.NewReader(data))
+	if err != nil {
+		return err
+	}
+	defer zsr.Close()
+
+	tr := tar.NewReader(zsr)
+
+	for {
+		header, err := tr.Next()
+		if err == io.EOF {
+			break // end of archive
+		}
+		if err != nil {
+			return err
+		}
+
+		path := filepath.Join(targetDir, header.Name)
+
+		switch header.Typeflag {
+		case tar.TypeDir:
+			if err := os.MkdirAll(path, os.FileMode(header.Mode)); err != nil {
+				return err
+			}
+
+		case tar.TypeReg:
+			// Ensure parent directories exist
+			if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
+				return err
+			}
+
+			outFile, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR|os.O_TRUNC, os.FileMode(header.Mode))
+			if err != nil {
+				return err
+			}
+
+			if _, err := io.Copy(outFile, tr); err != nil {
+				outFile.Close()
+				return err
+			}
+			outFile.Close()
+		}
+	}
+
+	return nil
+}
+
+func run() int {
+	tmpDirPkg, err := os.MkdirTemp("", "python-*")
+	if err != nil {
+		fmt.Println("Error creating temporary directory for Python standalone package:", err)
+		return 1
+	}
+	defer os.RemoveAll(tmpDirPkg)
+
+	if err := extractTarZst(pythonStandalonePkg, tmpDirPkg); err != nil {
+		fmt.Println("Error extracting Python standalone package:", err)
+		return 1
+	}
+	tmpDir, err := os.MkdirTemp("", "caddy-*")
+	if err != nil {
+		fmt.Println("Error creating temporary directory for Caddy binary:", err)
+		return 1
+	}
+	defer os.RemoveAll(tmpDir)
+
+	caddyPath := filepath.Join(tmpDir, "caddy")
+
+	if err := os.WriteFile(caddyPath, caddyBinary, 0755); err != nil {
+		fmt.Println("Error writing Caddy binary:", err)
+		return 1
+	}
+
+	if err := os.Chmod(caddyPath, 0755); err != nil {
+		fmt.Println("Error changing permissions for Caddy binary:", err)
+		return 1
+	}
+
+	args := os.Args[1:]
+
+	env := []string{}
+	for _, e := range os.Environ() {
+		if !strings.HasPrefix(e, "PYTHONHOME=") {
+			env = append(env, e)
+		}
+	}
+	env = append(env, fmt.Sprintf("PYTHONHOME=%s", tmpDir))
+
+	cmd := exec.Command(caddyPath, args...)
+	cmd.Env = env
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("Error running Caddy binary:", err)
+		return cmd.ProcessState.ExitCode()
+	}
+
+	return 0
+}
+
+func main() {
+	os.Exit(run())
+}
